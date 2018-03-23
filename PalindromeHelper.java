82d43f30826605faa2d7248d96a40a2edf980361
Ben Celsi <bencelsi@uw.edu>
2018-03-23 12:41:21 -0700

    show all option working

--- PalindromeHelper.java
+++ PalindromeHelper.java
@@ -5,23 +5,34 @@ import java.util.*;
 public class PalindromeHelper {
    public static void main (String[] args) throws FileNotFoundException{
       Scanner c = new Scanner(System.in);
-      Scanner d = new Scanner(new File("dictionary.txt"));
+      //Scanner d = new Scanner(new File("dictionary.txt"));
       Scanner e = new Scanner(new File("dictionary.txt"));
 	  ArrayList<String> al = new ArrayList<String>();
       while(e.hasNext()){
 		  al.add(e.next());
 	  }
       Object[] al2 = al.toArray();
-      //System.out.print(firstStartsWith(al.toArray(), "hi"));
-      //go(c, d);
+      String[] d = al.toArray(new String[al.size()]);
+      //System.out.print(d[firstStartsWith("a", d)]);
+      go(c, d);
    }
    
    
-   public static void go(Scanner c, Scanner d) throws FileNotFoundException{
+   public static void go(Scanner c, String[] d) throws FileNotFoundException{
       System.out.print("Find words that start with:");
-      String startsWith = c.next().toLowerCase(); 
-      System.out.print("1. show all \n 2. only show words where remaining letters reversed make a word");
-      Boolean showAll = (c.next() == "1");
+      String start = c.next().toLowerCase(); 
+      System.out.print("1. show all \n2. only show words where remaining letters reversed make a word\n");
+      Boolean showAll = c.next().equals("1");
+      
+      if (showAll) {
+    	  int i = firstStartsWith(d, start);
+    	  while (d[i].startsWith(start)) {
+    		  System.out.println(d[i]);
+    		  i++;
+    	  }
+    	  System.out.print("HI");
+      }
+      /*
       while(d.hasNext()){
          String word = d.next();
          if(word.startsWith(startsWith)){
@@ -43,25 +54,25 @@ public class PalindromeHelper {
                }
             }
          }
-      }
+      }*/
       System.out.println("-----DONE-----");     
    }
    
-   public static int firstStartsWith(String str, String[] d) {
-	   return fswHelper(str, d, 0, d.length);
+   public static int firstStartsWith(String[] d, String str) {
+	   return fswHelper(d, str, 0, d.length-1);
    }
    
    //finds the index of the first words that starts with the given input
-   public static int fswHelper(String str, String[] d, int l, int r) {
-	   int mid = l + (r-1)/2;
+   public static int fswHelper(String[] d, String str, int l, int r) {
+	   int mid = (l + r)/2;
 	   if (l > r) {
 		   return -1;
 	   } else if (d[mid].startsWith(str) && (mid == 0 || !d[mid-1].startsWith(str))) {
 		   return mid;
 	   } else if (d[mid].compareTo(str) < 0) {
-		   return fswHelper(str, d, mid+1, r);
+		   return fswHelper(d, str, mid+1, r);
 	   } else {
-		   return fswHelper(str, d, l, mid-1);   
+		   return fswHelper(d, str, l, mid-1);   
 	   }
    }
 }
